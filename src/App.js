179b3337e11df1f59aa7e6d31b35388c95b45583
MohamedIbrahim13 <mohamed.mohamed.yossef@gmail.com>
2021-11-05 10:44:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [firstChoice, setFirstchoice] = useState(null);
+  const [secondChoice, setSecondchoice] = useState(null);
+  const [disabled, setDisabed] = useState(false);
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }));
+    setFirstchoice(null);
+    setSecondchoice(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  const handleChoice = card => {
+    firstChoice ? setSecondchoice(card) : setFirstchoice(card);
+  };
+  const reset = () => {
+    setFirstchoice(null);
+    setSecondchoice(null);
+    setTurns(prev => prev + 1);
+    setDisabed(false);
+  };
+  useEffect(() => {
+    if (firstChoice && secondChoice) {
+      setDisabed(true);
+      if (firstChoice.src === secondChoice.src) {
+        //console.log("matched");
+        setCards(prevCard => {
+          return prevCard.map(card => {
+            if (card.src === firstChoice.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        reset();
+      } else {
+        //console.log("not matched");
+        setTimeout(() => reset(), 1500);
+      }
+    }
+  }, [firstChoice, secondChoice]);
+  useEffect(() => {
+    shuffleCards();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Cards Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={
+              card === firstChoice || card === secondChoice || card.matched
+            }
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>#Turns : {turns}</p>
     </div>
   );
 }
